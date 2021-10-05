a4a775ca946d5ef3f4584344f865fe06f4608676
ajommo <ajommorpg@gmail.com>
2021-10-05 18:56:18 +0200

    Initial commit.

---
+++ Controllers/CredentialsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Game.Authentication.Models;
+using Game.Database;
+
+namespace Game.Authentication.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CredentialsController
+    {
+        private readonly DatabaseContext database;
+
+        public CredentialsController(DatabaseContext database)
+        {
+            this.database = database;
+        }
+
+        [HttpPost]
+        [Route("credentials")]
+        public ActionResult<Credentials> PostCredentials([FromBody] Credentials credentials)
+        {
+            var account = database.GetAccount(credentials.Username, credentials.Password);
+            if (account == null)
+            {
+                return new UnauthorizedResult();
+            }
+            account.Token = Guid.NewGuid();
+            database.SaveChanges();
+            return new Credentials(account);
+        }
+
+        [HttpDelete]
+        [Route("credentials")]
+        public ActionResult<Credentials> DeleteCredentials([FromBody] Credentials credentials)
+        {
+            var account = database.GetAccount(credentials.Id, (Guid)credentials.Token);
+            if (account == null)
+            {
+                return new UnauthorizedResult();
+            }
+            account.Token = null;
+            database.SaveChanges();
+            return new Credentials(account);
+        }
+    }
+}
---
+++ Models/Credentials.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Game.Database.Models;
+
+namespace Game.Authentication.Models
+{
+    [DataContract]
+    public class Credentials
+    {
+        private int id;
+        private string username;
+        private string password;
+        private Guid? token;
+
+        public Credentials()
+        {
+        }
+
+        public Credentials(AccountEntity account)
+        {
+            id = account.Id;
+            username = account.Username;
+            token = account.Token;
+        }
+
+        [DataMember(Name = "id")]
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        [DataMember(Name = "username")]
+        public string Username
+        {
+            get => username;
+            set => username = value;
+        }
+
+        [DataMember(Name = "password")]
+        public string Password
+        {
+            get => password;
+            set => password = value;
+        }
+
+        [DataMember(Name = "token")]
+        public Guid? Token
+        {
+            get => token;
+            set => token = value;
+        }
+    }
+}
